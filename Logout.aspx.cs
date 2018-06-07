94e267d4636e0e566981e15b291821c07413f841
Niemeart <40023354+Niemeart@users.noreply.github.com>
2018-06-07 15:09:47 +0300

    Update Logout.aspx.cs

--- Logout.aspx.cs
+++ Logout.aspx.cs
@@ -13,7 +13,6 @@ public partial class Logout : System.Web.UI.Page
         Session.Abandon();
         Session.Remove("Username");
         FormsAuthentication.SignOut();
-        // Response.Redirect(Request.UrlReferrer.ToString());
         Response.Redirect("Logout.aspx");
     }
 }
