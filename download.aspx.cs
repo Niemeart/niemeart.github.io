b8ca8935ab482803a4c6c908b96ccdac70958bd2
Niemeart <40023354+Niemeart@users.noreply.github.com>
2018-06-07 15:10:32 +0300

    Update download.aspx.cs

--- download.aspx.cs
+++ download.aspx.cs
@@ -23,10 +23,10 @@ public partial class download : System.Web.UI.Page
             if (fileName != "" && fileName.IndexOf(".") > 0)
             {
                 bool extensionAllowed = false;
-                // get file extension
+
                 string fileExtension = fileName.Substring(fileName.LastIndexOf('.'), fileName.Length - fileName.LastIndexOf('.'));
 
-                // check that we are allowed to download this file extension
+
                 string[] extensions = allowedExtensions.Split(',');
                 for (int a = 0; a < extensions.Length; a++)
                 {
@@ -39,17 +39,15 @@ public partial class download : System.Web.UI.Page
 
                 if (extensionAllowed == false)
                 {
-                    // check to see that the file exists 
+
                     if (File.Exists(Server.MapPath(filePath + '/' + fileName)))
                     {
 
-                        // for iphones and ipads, this script can cause problems - especially when trying to view videos, so we will redirect to file if on iphone/ipad
                         if (Request.UserAgent.ToLower().Contains("iphone") || Request.UserAgent.ToLower().Contains("ipad")) { Response.Redirect(filePath + '/' + fileName); }
 
                         Response.Clear();
                         int lastKenoPos = fileName.LastIndexOf('/');
                         string trimFileName = fileName.Substring(lastKenoPos + 1);
-                        //Response.AddHeader("content-disposition", "attachment;filename=" + fileName);
                         Response.AddHeader("Content-Disposition", "attachment;filename=" + trimFileName);
                         Response.TransmitFile(Server.MapPath(filePath + '/' + fileName));
                         if ((Page.User.IsInRole("User")) || (Page.User.IsInRole("HRMAditrolta")) || (Page.User.IsInRole("HRMAditrolle")) || (Page.User.IsInRole("FRMAditrolta")) || (Page.User.IsInRole("FRMAditrolle")))
